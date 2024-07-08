75c55dbd66cb73af44d0dba7cb2fd13a85652915
Arnis Prieditis <arnis.prieditis19@gmail.com>
2024-07-08 17:25:04 +0300

    add error handling

--- kilo.c
+++ kilo.c
@@ -3,32 +3,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
+#include <errno.h>
 
 struct termios original_termios;
 
+void die(const char * s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &original_termios);
+    if (tcgetattr(STDIN_FILENO, &original_termios) == -1)
+        die("tcgetattr");
     atexit(disableRawMode);
     struct termios raw = original_termios;
     // some flags to modify the behaviour of the terminal
+    // https://www.man7.org/linux/man-pages/man3/termios.3.html for more info
     raw.c_iflag &= ~(IXON | ICRNL | INPCK | ISTRIP | BRKINT);
     raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
     raw.c_cflag |= (CS8);
     raw.c_cc[VMIN] = 0;     //sets the minimum number of bytes of input needed before read() can return
     raw.c_cc[VTIME] = 1;    // tenths of a second; time to wait before read() returns
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
 }
 
 int main() {
     enableRawMode();
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) // when read() times out, EAGAIN is thrown
+            die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
